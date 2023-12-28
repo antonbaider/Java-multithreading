ceb8238576ae71161556b7793a0846af5d82dc0f
antonbaider <email.b.anton@gmail.com>
2023-12-28 13:50:14 +0100

    done

--- src/main/java/task1/ParallelCalculator.java
+++ src/main/java/task1/ParallelCalculator.java
@@ -2,4 +2,21 @@ package task1;
 
 import java.util.function.BinaryOperator;
 
-// Write your code here
+public class ParallelCalculator implements Runnable {
+
+    int result;
+    private BinaryOperator<Integer> operator;
+    private int operand1;
+    private int operand2;
+
+    public ParallelCalculator(BinaryOperator<Integer>, int operand1, int operand2) {
+        this.operator = operator;
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    @Override
+    public void run() {
+        result = operator.apply(operand1, operand2);
+    }
+}
